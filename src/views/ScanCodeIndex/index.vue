fd1938d50bcaa8da119a4fa903b619383f11ee1e
zhuxiaoyi <zhuxiaoyi@163.com>
2025-04-16 20:24:12 +0800

    feat: 更换公众号

--- src/views/ScanCodeIndex/index.vue
+++ src/views/ScanCodeIndex/index.vue
@@ -96,7 +96,7 @@ export default {
         if (code === 200) {
           wx.config({
             // debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-            appId: 'wx054c52e568b730c4', // 必填，公众号的唯一标识
+            appId: 'wx92603ad3266e6afb', // 必填，公众号的唯一标识
             timestamp: data.timestamp, // 必填，生成签名的时间戳
             nonceStr: data.nonceStr, // 必填，生成签名的随机串
             signature: data.signature, // 必填，签名
